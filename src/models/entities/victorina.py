5b7d9b1ab3dbf1f481744a18730efc83c7a67a45
Rustam-Gazizulin <rgazizulin174@gmail.com>
2023-05-24 12:08:12 +0500

    Изменыл вывод даты на стандартный дд.мм.гг

--- src/models/entities/victorina.py
+++ src/models/entities/victorina.py
@@ -1,3 +1,5 @@
+from src.utils.date_format import DateFormat
+
 class Questions:
 
     def __init__(self, id, text_question=None, answer_question=None, date_created=None) -> None:
@@ -11,5 +13,5 @@ class Questions:
             'id': self.id,
             'text_question': self.text_question,
             'answer_question': self.answer_question,
-            'date_created': self.date_created
+            'date_created': DateFormat.convert_date(self.date_created)
         }
---
+++ src/utils/date_format.py
@@ -0,0 +1,9 @@
+import datetime
+
+
+# Функция редактирования вывода данных типа DateTime
+
+class DateFormat():
+    @classmethod
+    def convert_date(self, date):
+        return datetime.datetime.strftime(date, '%d/%m/%Y')

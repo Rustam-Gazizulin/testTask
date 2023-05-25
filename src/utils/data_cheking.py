673871a91595ac366d3f7a1d94110fca75c9df87
Rustam-Gazizulin <rgazizulin174@gmail.com>
2023-05-25 19:12:49 +0500

    Добавил фильтрацию получаемых данных чтобы избежать дублирования вопросов в базе данных

---
+++ src/utils/data_cheking.py
@@ -0,0 +1,17 @@
+from src.database.db import get_connection
+
+
+# Функция для получения списка уникальных id из Базы данных
+def get_id_database():
+    id_database = []
+    connection = get_connection()
+    with connection.cursor() as cursor:
+        cursor.execute(
+            "SELECT id FROM questions")
+        resultset = cursor.fetchall()
+    for num in resultset:
+        id_database.append(num[0])
+
+    return id_database
+
+
--- src/utils/request_data.py
+++ src/utils/request_data.py
@@ -1,22 +1,35 @@
 import requests
 
+from src.utils.data_cheking import get_id_database
+
 
 # Функция запроса данных с АПИ и конвертация для БД
 def request_to_api(url_api: str, count_questions: int):
-    response = requests.get(f'{url_api}{count_questions}')
-    data = response.json()
+    id_database = get_id_database()
     output_data = []
-    for obj in data:
-        id = obj.get("id")
-        text_question = obj.get("question")
-        answer_question = obj.get("answer")
-        date_created = obj.get("created_at")
-        extracted_data = {
-            "id": id,
-            "text_question": text_question,
-            "answer_question": answer_question,
-            "date_created": date_created
-        }
-        output_data.append(extracted_data)
+    while int(count_questions) > 0:
+        response = requests.get(f'{url_api}{count_questions}')
+        data = response.json()
+        count = 0
+        for obj in data:
+            if not obj.get("id") in id_database:
+                id = obj.get("id")
+                text_question = obj.get("question")
+                answer_question = obj.get("answer")
+                date_created = obj.get("created_at")
+                extracted_data = {
+                    "id": id,
+                    "text_question": text_question,
+                    "answer_question": answer_question,
+                    "date_created": date_created
+                }
+                output_data.append(extracted_data)
+            else:
+                count += 1
+                continue
+        if count > 0:
+            count_questions = count
+        else:
+            count_questions = 0
     return output_data
 

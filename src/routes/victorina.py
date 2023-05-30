53f70d1beee7230da4ba00440a6aa2a60bf9849d
Rustam-Gazizulin <rgazizulin174@gmail.com>
2023-05-30 13:40:12 +0500

    add retrieve page

--- src/routes/victorina.py
+++ src/routes/victorina.py
@@ -26,7 +26,7 @@ def get_question(id):
     try:
         question = VictorinaModel.get_question(id)
         if question is not None:
-            return jsonify(question)
+            return render_template('retrieve_page.html', quest=question)
         else:
             return jsonify({"error": "Такого вопроса нет в базе данных"}), 404
 
@@ -34,29 +34,33 @@ def get_question(id):
         return jsonify({'message': str(ex)}), 500
 
 
-# Добавление вопроса в БД самостоятельно
-@main.route('/add/<count>', methods=['POST'])
-def add_question(count):
-    list_question = request_to_api(api_url, count)
-    for quest in list_question:
-        try:
-            answer_question = quest['answer_question']
-            text_question = quest['text_question']
-            date_created = quest['date_created']
-            id = quest['id']
-            question = Questions(id, answer_question, text_question, date_created)
+# Добавление вопросов в БД
+@main.route('/add', methods=['POST', 'GET'])
+def add_question():
+    if request.method == 'POST':
+        count = request.form['count']
+        if count != '':
+            list_question = request_to_api(api_url, int(count))
+            for quest in list_question:
+                try:
+                    answer_question = quest['answer_question']
+                    text_question = quest['text_question']
+                    date_created = quest['date_created']
+                    id = quest['id']
+                    question = Questions(id, answer_question, text_question, date_created)
 
-            affected_rows = VictorinaModel.add_question(question)
+                    affected_rows = VictorinaModel.add_question(question)
 
-            if affected_rows == 1:
-                continue
-            else:
-                return jsonify({"message": "Ошибка создания вопроса проверьте корректность полей"}), 500
-
-        except Exception as ex:
-            return jsonify({'message': str(ex)}), 500
-    return 'Вопросы сгенерированы'
-
+                    if affected_rows == 1:
+                        continue
+                    else:
+                        return jsonify({"message": "Ошибка создания вопроса проверьте корректность полей"}), 500
+                except Exception as ex:
+                    return jsonify({'message': str(ex)}), 500
+        else:
+            return render_template('generate.html')
+        return render_template('finish_generate.html')
+    return render_template('generate.html')
 
 # Обновление данных существующего вопроса
 @main.route('/update/<id>', methods=['PUT'])
